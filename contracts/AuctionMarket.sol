725ea3d00805c1d898537b7a4c7d4d37bbb15f67
Administrator <744890753@qq.com>
2026-01-27 22:59:19 +0800

    styles: 添加solidity格式化配置文件
    - 添加solhint.json
    - 添加settings.json

---
+++ contracts/AuctionMarket.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC20.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+contract AuctionMarket is ReetrancyGuard, Ownable {
+    
+}
