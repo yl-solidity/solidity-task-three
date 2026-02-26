6eee5ca9b651a8ba449737eca85cab023b06c541
Administrator <744890753@qq.com>
2026-02-26 14:49:27 +0800

    refactor(AuctionMarketV2.sol):解决引入ReentrancyGuardUpgradeable报错问题
    - 路径在secureity下，不是utils

--- contracts/AuctionMarketV2.sol
+++ contracts/AuctionMarketV2.sol
@@ -5,7 +5,7 @@ import "./AuctionMarket.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
 
 contract AuctionMarketV2 is Initializable, OwnableUpgradeable, UUPSUpgradeable, ReentrancyGuardUpgradeable {
     // 动态手续费结构
@@ -36,7 +36,7 @@ contract AuctionMarketV2 is Initializable, OwnableUpgradeable, UUPSUpgradeable, 
      * 初始化
      */
     function initialize() initializer public {
-        __Ownable_init(msg.sender);
+        __Ownable_init();
         __UUPSUpgradeable_init();
         __ReentrancyGuard_init();
 
